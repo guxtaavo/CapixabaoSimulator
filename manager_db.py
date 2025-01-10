98a2f9d8e15bf4802f96c70d302ef55168fad0b4
Gustavo Nunes Lopes <gustaavo.nunesl@gmail.com>
2025-01-10 11:24:21 -0300

    Visualizando e criando as coisas

--- manager_db.py
+++ manager_db.py
@@ -1,6 +1,8 @@
 import sqlite3
 from pathlib import Path
+from tabulate import tabulate
 from clube import Clube
+import os
 
 # Configuração do caminho do banco de dados
 ROOT_DIR = Path(__file__).parent
@@ -19,6 +21,7 @@ class ManagerDB:
             f"""
             CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                posicao INTEGER,
                 nome_do_time TEXT,
                 pontos INTEGER,
                 jogos INTEGER,
@@ -28,8 +31,7 @@ class ManagerDB:
                 gols_pro INTEGER,
                 gols_contra INTEGER,
                 saldo_de_gols INTEGER,
-                aproveitamento INTEGER,
-                posicao INTEGER
+                aproveitamento INTEGER
             )
             """
         )
@@ -38,14 +40,49 @@ class ManagerDB:
 
     @classmethod
     def visualizar_tabela(cls):
+        os.system('cls')
         con = sqlite3.connect(DB_FILE)
         cursor = con.cursor()
-        cursor.execute(f'SELECT nome_do_time, pontos, jogos, vitorias, empates, derrotas, gols_pro, gols_contra, saldo_de_gols, aproveitamento, posicao FROM {TABLE_NAME}')
+        cursor.execute(f'SELECT * FROM {TABLE_NAME} ORDER BY pontos DESC, vitorias DESC, saldo_de_gols DESC, gols_pro DESC')
         rows = cursor.fetchall()
 
-        for row in rows:
-            print(row)
-        
+        dados = [
+            [
+                i + 1,  # Posição
+                row[2],  # Nome do Time
+                row[3],  # Pontos
+                row[4],  # Jogos
+                row[5],  # Vitórias
+                row[6],  # Empates
+                row[7],  # Derrotas
+                row[8],  # Gols Pró
+                row[9],  # Gols Contra
+                row[10],  # Saldo de Gols
+                f'{row[11]}%'  # Aproveitamento
+            ]
+            for i, row in enumerate(rows)
+        ]
+
+        cabecalho = [
+            "Posição", "Nome do Time", "Pontos", "Jogos", "Vitórias",
+            "Empates", "Derrotas", "Gols Pró", "Gols Contra", "Saldo de Gols", "Aproveitamento"
+        ]
+
+        tabela_formatada = tabulate(dados, headers=cabecalho, tablefmt="grid")
+        print(tabela_formatada)
+
+        con.close()
+
+    @classmethod
+    def atualizar_posicoes(cls):
+        con = sqlite3.connect(DB_FILE)
+        cursor = con.cursor()
+        cursor.execute(f'SELECT id FROM {TABLE_NAME} ORDER BY pontos DESC, vitorias DESC, saldo_de_gols DESC, gols_pro DESC')
+        rows = cursor.fetchall()
+
+        for nova_posicao, (time_id,) in enumerate(rows, start=1):
+            cursor.execute(f"UPDATE {TABLE_NAME} SET posicao = ? WHERE id = ?", (nova_posicao, time_id))
+        con.commit()
         con.close()
 
     @classmethod
@@ -56,10 +93,10 @@ class ManagerDB:
         count = cursor.fetchone()[0]
         posicao = count + 1
         sql = (
-            f"INSERT INTO {TABLE_NAME} (nome_do_time, pontos, jogos, vitorias, empates, derrotas, gols_pro, gols_contra, saldo_de_gols, aproveitamento, posicao) "
+            f"INSERT INTO {TABLE_NAME} (posicao, nome_do_time, pontos, jogos, vitorias, empates, derrotas, gols_pro, gols_contra, saldo_de_gols, aproveitamento) "
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
         )
-        cursor.execute(sql, (clube.nome, clube.pontos, clube.jogos, clube.vitorias, clube.empates, clube.derrotas, clube.gols_pro, clube.gols_contra, clube.saldo_gols, clube.aproveitamento, posicao))
+        cursor.execute(sql, (posicao, clube.nome, clube.pontos, clube.jogos, clube.vitorias, clube.empates, clube.derrotas, clube.gols_pro, clube.gols_contra, clube.saldo_gols, clube.aproveitamento))
         con.commit()
         con.close()
 
@@ -68,7 +105,7 @@ class ManagerDB:
         rodada = []
         con = sqlite3.connect(DB_FILE)
         cursor = con.cursor()
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS Rodada_{numero_rodada} (id INTEGER PRIMARY KEY AUTOINCREMENT, time1_id INTEGER, time1_nome TEXT, time2_id INTEGER, time2_nome TEXT, placar_time1 INTEGER, placar_time2 INTEGER)")
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS Rodada_{numero_rodada} (id INTEGER PRIMARY KEY AUTOINCREMENT, time1_id INTEGER, time1_nome TEXT, placar_time1 INTEGER, time2_id INTEGER, time2_nome TEXT, placar_time2 INTEGER)")
         con.commit()
 
         for i in range(5):
@@ -96,7 +133,7 @@ class ManagerDB:
             clube2 = Clube.from_db_row(row2)
 
             rodada.append([clube1.nome, clube2.nome])
-            cursor.execute(f"INSERT INTO Rodada_{numero_rodada} (time1_id, time1_nome, time2_id, time2_nome, placar_time1, placar_time2) VALUES (?, ?, ?, ?, ?, ?)", (row1[0], clube1.nome, None, row2[0], clube2.nome, None))
+            cursor.execute(f"INSERT INTO Rodada_{numero_rodada} (time1_id, time1_nome, placar_time1, time2_id, time2_nome, placar_time2) VALUES (?, ?, ?, ?, ?, ?)", (row1[0], clube1.nome, None, row2[0], clube2.nome, None))
             con.commit()
 
         cls.rodadas.append(rodada)
@@ -110,20 +147,75 @@ class ManagerDB:
             cls.adicionar_rodada(i)
 
     @classmethod
-    def atualizar_rodada(cls):
-        pass
+    def carregar_campeonato(cls):
+        con = sqlite3.connect(DB_FILE)
+        cursor = con.cursor()
+        cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{TABLE_NAME}'")
+        table_exists = cursor.fetchone()
+        con.close()
+        if table_exists:
+            print("Campeonato carregado com sucesso.")
+            return True
+        else:
+            print("Nenhum campeonato encontrado.")
+            return None
+
+    @classmethod
+    def atualizar_rodada(cls, numero_rodada, jogo, placar_time1, placar_time2):
+        con = sqlite3.connect(DB_FILE)
+        cursor = con.cursor()
+        cursor.execute(f"UPDATE Rodada_{numero_rodada} SET placar_time1 = ?, placar_time2 = ? WHERE id = ?", (placar_time1, placar_time2, jogo))
+        con.commit()
+        con.close()
+        cls.atualizar_estatisticas(numero_rodada)
+
+    @classmethod
+    def atualizar_estatisticas(cls):
+        con = sqlite3.connect(DB_FILE)
+        cursor = con.cursor()
+        cursor.execute(f"SELECT * FROM {TABLE_NAME}")
+        times = cursor.fetchall()
+
+        for time in times:
+            time_id = time[0]
+            cursor.execute(f"SELECT SUM(placar_time1), SUM(placar_time2) FROM Rodada WHERE time1_id = ? OR time2_id = ?", (time_id, time_id))
+            gols_pro, gols_contra = cursor.fetchone()
+            gols_pro = gols_pro if gols_pro is not None else 0
+            gols_contra = gols_contra if gols_contra is not None else 0
+
+            cursor.execute(f"SELECT COUNT(*) FROM Rodada WHERE (time1_id = ? AND placar_time1 > placar_time2) OR (time2_id = ? AND placar_time2 > placar_time1)", (time_id, time_id))
+            vitorias = cursor.fetchone()[0]
+
+            cursor.execute(f"SELECT COUNT(*) FROM Rodada WHERE (time1_id = ? AND placar_time1 < placar_time2) OR (time2_id = ? AND placar_time2 < placar_time1)", (time_id, time_id))
+            derrotas = cursor.fetchone()[0]
+
+            cursor.execute(f"SELECT COUNT(*) FROM Rodada WHERE (time1_id = ? OR time2_id = ?) AND placar_time1 = placar_time2", (time_id, time_id))
+            empates = cursor.fetchone()[0]
+
+            jogos = vitorias + derrotas + empates
+            pontos = vitorias * 3 + empates
+            saldo_de_gols = gols_pro - gols_contra
+            aproveitamento = round((pontos / (jogos * 3)) * 100) if jogos > 0 else 0
+
+            cursor.execute(f"UPDATE {TABLE_NAME} SET pontos = ?, jogos = ?, vitorias = ?, empates = ?, derrotas = ?, gols_pro = ?, gols_contra = ?, saldo_de_gols = ?, aproveitamento = ? WHERE id = ?", (pontos, jogos, vitorias, empates, derrotas, gols_pro, gols_contra, saldo_de_gols, aproveitamento, time_id))
+            con.commit()
+
+        con.close()
+
+        cls.atualizar_posicoes()
 
 if __name__ == "__main__":
-    ManagerDB.create_table()
-    ManagerDB.adicionar_time(Clube("Capixaba"))
-    ManagerDB.adicionar_time(Clube("Desportiva"))
-    ManagerDB.adicionar_time(Clube("Jaguaré"))
-    ManagerDB.adicionar_time(Clube("Nova Venécia"))
-    ManagerDB.adicionar_time(Clube("Porto Vitória"))
-    ManagerDB.adicionar_time(Clube("Real Noroeste"))
-    ManagerDB.adicionar_time(Clube("Rio Branco VN"))
-    ManagerDB.adicionar_time(Clube("Rio Branco SAF"))
-    ManagerDB.adicionar_time(Clube("Vilavelhense"))
-    ManagerDB.adicionar_time(Clube("Vitória"))
-    ManagerDB.criar_rodadas()
-    ManagerDB.visualizar_tabela()
+    ...
+    # ManagerDB.create_table()
+    # ManagerDB.adicionar_time(Clube("Capixaba"))
+    # ManagerDB.adicionar_time(Clube("Desportiva"))
+    # ManagerDB.adicionar_time(Clube("Jaguaré"))
+    # ManagerDB.adicionar_time(Clube("Nova Venécia"))
+    # ManagerDB.adicionar_time(Clube("Porto Vitória"))
+    # ManagerDB.adicionar_time(Clube("Real Noroeste"))
+    # ManagerDB.adicionar_time(Clube("Rio Branco VN"))
+    # ManagerDB.adicionar_time(Clube("Rio Branco SAF"))
+    # ManagerDB.adicionar_time(Clube("Vilavelhense"))
+    # ManagerDB.adicionar_time(Clube("Vitória"))
+    # ManagerDB.criar_rodadas()
+    # ManagerDB.visualizar_tabela()
--- terminal_interface.py
+++ terminal_interface.py
@@ -1,30 +1,89 @@
 from clube import Clube
-import sys
+from manager_db import ManagerDB
+import sys, os
 
-class TerminalInterface():
+class TerminalInterface:
     def __init__(self):
         self.menu()
 
     def menu(self):
         while True:
+            os.system('cls')
             print("Bem-vindo ao sistema de controle de campeonato!")
+            print("Escolha uma opção:")
+            print("1 - Criar campeonato")
+            print("2 - Entrar em campeonato existente")
+            print("3 - Sair")
+            opcao = input("Digite o número da opção desejada: ")
+            match opcao:
+                case "1":
+                    os.system('cls')
+                    self.criar_campeonato()
+                case "2":
+                    os.system('cls')
+                    self.entrar_campeonato()
+                case "3":
+                    os.system('cls')
+                    print("Obrigado por utilizar o sistema!")
+                    sys.exit()
+                case _:
+                    print("Opção inválida!")
+
+    def criar_campeonato(self):
+        ManagerDB.create_table()
+        while True:
             print("Escolha uma opção:")
             print("1 - Cadastrar clube")
             print("2 - Listar clubes")
             print("3 - Cadastrar rodada")
             print("4 - Listar rodadas")
-            print("5 - Sair")
+            print("5 - Voltar ao menu principal")
             opcao = input("Digite o número da opção desejada: ")
             match opcao:
                 case "1":
+                    os.system('cls')
                     self.cadastrar_clube()
                 case "2":
-                    self.listar_clubes()
+                    os.system('cls')
+                    self.visualizar_tabela()
                 case "3":
+                    os.system('cls')
                     self.cadastrar_rodada()
                 case "4":
+                    os.system('cls')
                     self.listar_rodadas()
                 case "5":
+                    return
+                case _:
+                    print("Opção inválida!")
+
+    def entrar_campeonato(self):
+        self.menu_campeonato()
+        # print("Entrando em campeonato existente...")
+        # nome_do_campeonato = input("Digite o nome do campeonato: ")
+        # campeonato = ManagerDB.carregar_campeonato(nome_do_campeonato)
+        # if campeonato is None:
+        #     print("Nenhum campeonato encontrado!")
+        #     return
+        # elif campeonato is True: 
+        #     self.menu_campeonato()
+        # else:
+        #     print("Erro inesperado!")
+        #     sys.exit()
+
+    def menu_campeonato(self):
+        while True:
+            print("Escolha uma opção:")
+            print("1 - Mostrar classificação")
+            print("2 - Editar rodada")
+            print("3 - Sair")
+            opcao = input("Digite o número da opção desejada: ")
+            match opcao:
+                case "1":
+                    self.visualizar_tabela()
+                case "2":
+                    ManagerDB.editar_rodada()
+                case "3":
                     print("Obrigado por utilizar o sistema!")
                     sys.exit()
                 case _:
@@ -32,45 +91,26 @@ class TerminalInterface():
 
     def cadastrar_clube(self):
         while True:
-            nome = input("Digite o nome do clube: ")
-            apelido = input("Digite o apelido do clube: ")
-            clube = Clube(nome, apelido)
-            print(clube)
-            resposta = input("Deseja cadastrar mais um clube? (s/n) ")
-            if resposta.lower() == 'n':
+            os.system('cls')
+            numero_clubes = int(input("Digite o número de clubes a serem cadastrados: "))
+            if numero_clubes != 0:
                 break
+            print("Número inválido. Tente novamente.")
+        for _ in range(numero_clubes):
+            nome = input("Digite o nome do clube: ")
+            clube = Clube(nome)
+            ManagerDB.adicionar_time(clube)
 
-    def listar_clubes(self):
-        sys.quit()
+    def visualizar_tabela(self):
+        ManagerDB.visualizar_tabela()
 
     def cadastrar_rodada(self):
-        sys.quit()
-        # while True:
-        #     rodada = []
-        #     i = 0
-        #     print("Cadastrando rodada")
-        #     for clube in clubes:
-        #         i+=1
-        #         print(f"{i}. {clube}")
-        #     while True:
-        #         indice_clube_casa = int(input("Digite o número do clube da casa: "))
-        #         if indice_clube_casa < 1 or indice_clube_casa > len(clubes):
-        #             print("Clube inválido!")
-        #         else:
-        #             clube_casa = clubes[indice_clube_casa-1]
-        #             break
-        #     while True:
-        #         indice_clube_visitante = int(input("Digite o número do clube visitante: "))
-        #         if indice_clube_visitante < 1 or indice_clube_visitante > len(clubes) or indice_clube_visitante == indice_clube_casa:
-        #             print("Clube inválido!")
-        #         else:
-        #             clube_visitante = clubes[indice_clube_visitante-1]
-        #             break
-        #     rodada.append([clube_casa, clube_visitante])
-
+        numero_rodada = input("Digite o número da rodada: ")
+        ManagerDB.adicionar_rodada(numero_rodada)
 
     def listar_rodadas(self):
-        sys.quit()
+        for i, rodada in enumerate(ManagerDB.rodadas, start=1):
+            print(f"Rodada {i}: {rodada}")
 
-if __name__ == '__main__':
-    terminal = TerminalInterface()
+if __name__ == "__main__":
+    TerminalInterface()
